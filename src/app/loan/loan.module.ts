131f9f1420a2935c27943f649630b8b3c19ba55a
Dmytro Riazantsev <dsr.workk@gmail.com>
2020-06-02 15:43:17 +0300

    - rename container -> containers

--- src/app/loan/loan.module.ts
+++ src/app/loan/loan.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoanContainerComponent } from './container/loan-container/loan-container.component';
+import { LoanContainerComponent } from './containers/loan-container/loan-container.component';
 import { LoanItemComponent } from './components/loan-item/loan-item.component';
 import { InvestmentBalanceComponent } from './components/investment-balance/investment-balance.component';
 
